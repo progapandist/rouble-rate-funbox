e061818a7470b7bde473d8bdfdd8925a352464fb
Andy B <andybarnov@gmail.com>
2016-12-27 09:52:51 +0300

    scrape the right rate from cbr.ru

--- app/services/cb_rate_finder.rb
+++ app/services/cb_rate_finder.rb
@@ -5,7 +5,7 @@ class CBRateFinder
   def self.dollar
     url = "http://cbr.ru/"
     parsed = Nokogiri::HTML(open(url))
-    found = parsed.search('#widget_exchange .weak')
+    found = parsed.search('#widget_exchange .w_data_wrap')
     rate_str = found.first.text.gsub(/[^0-9,]+/, '')
     rate_str.gsub(',', '.').to_f
   end
